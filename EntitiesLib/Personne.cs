49475b4cb2eb254438801a0efb5509dc78e96604
Unknown <lancelot.duisit@gmail.com>
2018-01-31 11:42:39 -0500

    ajout UI et modif entities
    ajout UI et modif entities

--- EntitiesLib/Personne.cs
+++ EntitiesLib/Personne.cs
@@ -16,5 +16,186 @@ namespace EntitiesLib
         private int id_categoriePersonne;
         private int id_coordonnees;
         private int id_adresse;
+
+        private Profession profession;
+        private CategoriePersonne categorie;
+        private Coordonnees coord;
+        private Adresse adresse;
+
+        public Personne() { }
+
+        public Personne(int id, string nom, string prenom, int age, int id_profession, int id_categoriePersonne, int id_coordonnees, int id_adresse, Profession profession, CategoriePersonne categorie, Coordonnees coord, Adresse adresse)
+        {
+            this.Id = id;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Age = age;
+            this.Id_profession = id_profession;
+            this.Id_categoriePersonne = id_categoriePersonne;
+            this.Id_coordonnees = id_coordonnees;
+            this.Id_adresse = id_adresse;
+            this.Profession = profession;
+            this.Categorie = categorie;
+            this.Coord = coord;
+            this.Adresse = adresse;
+        }
+
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        public int Id_profession
+        {
+            get
+            {
+                return id_profession;
+            }
+
+            set
+            {
+                id_profession = value;
+            }
+        }
+
+        public int Id_categoriePersonne
+        {
+            get
+            {
+                return id_categoriePersonne;
+            }
+
+            set
+            {
+                id_categoriePersonne = value;
+            }
+        }
+
+        public int Id_coordonnees
+        {
+            get
+            {
+                return id_coordonnees;
+            }
+
+            set
+            {
+                id_coordonnees = value;
+            }
+        }
+
+        public int Id_adresse
+        {
+            get
+            {
+                return id_adresse;
+            }
+
+            set
+            {
+                id_adresse = value;
+            }
+        }
+
+        internal Profession Profession
+        {
+            get
+            {
+                return profession;
+            }
+
+            set
+            {
+                profession = value;
+            }
+        }
+
+        internal CategoriePersonne Categorie
+        {
+            get
+            {
+                return categorie;
+            }
+
+            set
+            {
+                categorie = value;
+            }
+        }
+
+        internal Coordonnees Coord
+        {
+            get
+            {
+                return coord;
+            }
+
+            set
+            {
+                coord = value;
+            }
+        }
+
+        internal Adresse Adresse
+        {
+            get
+            {
+                return adresse;
+            }
+
+            set
+            {
+                adresse = value;
+            }
+        }
+
     }
 }
--- EntitiesLib/Profession.cs
+++ EntitiesLib/Profession.cs
@@ -12,5 +12,67 @@ namespace EntitiesLib
         private string titre;
         private string nomEntreprise;
         private int id_personne;
+
+
+        public Profession(int id, string titre, string nomEntreprise, int id_personne)
+        {
+            this.id = id;
+            this.titre = titre;
+            this.nomEntreprise = nomEntreprise;
+            this.id_personne = id_personne;
+        }
+
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Titre
+        {
+            get
+            {
+                return titre;
+            }
+
+            set
+            {
+                titre = value;
+            }
+        }
+
+        public string NomEntreprise
+        {
+            get
+            {
+                return nomEntreprise;
+            }
+
+            set
+            {
+                nomEntreprise = value;
+            }
+        }
+
+        public int Id_personne
+        {
+            get
+            {
+                return id_personne;
+            }
+
+            set
+            {
+                id_personne = value;
+            }
+        }
     }
 }
